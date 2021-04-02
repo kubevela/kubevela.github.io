8c99f0c3e85889e8240bf5ebc0be39193324248e
Lei Zhang <resouer@gmail.com>
2021-04-01 18:39:44 -0700

    Fix typo
    Final message

--- src/data/features.js
+++ src/data/features.js
@@ -3,14 +3,14 @@ import Translate, { translate } from '@docusaurus/Translate';
 
 const features = [
   {
-    title: <>Application Centric</>,
+    title: <>Developer Centric</>,
     imgUrl: 'img/application-centric.svg',
     description: (
       <>
         <p>
           <Translate>
             KubeVela introduces the </Translate>
-            <i>Application</i> <Translate>concept to capture a full deployment of microservices, and build operational features
+            <i>Application</i> <Translate>as the main API to capture a full deployment of microservices, and builds features
              around the application needs only. Progressive rollout 
             and multi-cluster deployment are provided out-of-box. No infrastructure level concerns, simply deploy.
           </Translate>
@@ -19,17 +19,16 @@ const features = [
     ),
   },
   {
-    title: <>A Extensible and Self-Service Platform</>,
+    title: <>Templates Based Self-Service</>,
     imgUrl: 'img/extending-natively.svg',
     description: (
       <>
         <p>
           <Translate>
-            Features of KubeVela are abstracted as LEGO-sytle modules with leverage of
-            </Translate><a href="https://github.com/cuelang/cue"> CUELang</a> <Translate>and </Translate>
-           <a href="https://helm.sh/">Helm</a><Translate>. This enables you to assemble 
-           any infrastructure capabilities per needs of the application, and ship them to end users in self-service approach.
-           Natively extensible, no abstraction leak.
+           In KubeVela, all deployable workloads, cloud services and operational features are abstracted as LEGO-sytle templates via
+             </Translate><a href="https://github.com/cuelang/cue"> CUE</a> <Translate>and/or </Translate>
+           <a href="https://helm.sh/">Helm</a><Translate>. This enables the end-users to declare
+            an application deployment by picking and assembling templates per their own need. No restrictions, natively extensible.
           </Translate>
         </p>
       </>
@@ -37,13 +36,13 @@ const features = [
     reverse: true,
   },
   {
-    title: <>Simple yet Reliable Abstractions</>,
+    title: <>Simple yet Reliable</>,
     imgUrl: 'img/simple-yet-extensible-abstraction-mechanism.svg',
     description: (
       <>
         <p>
           <Translate>
-            All abstractions in KubeVela are managed with 
+            Defined by CUE and Helm, abstractions in KubeVela are essentially maintained with 
             </Translate><a href="https://kubernetes.io/docs/concepts/architecture/controller/"> Kubernetes 
             Control Loop</a> <Translate> so they will never leave </Translate> <i>Configuration Drift</i> <Translate>in 
             your cluster. Also, as a set of </Translate><a href="https://kubernetes.io/docs/concepts/extend-kubernetes/api-extension/custom-resources/">
