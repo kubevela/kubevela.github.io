8f8268d7a16e7dad0f112c6386e6e2ed1476e9b4
guoxudong <guoxudong.dev@gmial.com>
2021-04-08 15:39:34 +0800

    add netfly

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -105,6 +105,15 @@ module.exports = {
             },
           ],
         },
+        {
+          title: 'Others',
+          items: [
+            {
+              html:
+                '<a href="https://www.netlify.com"><img src="https://www.netlify.com/img/global/badges/netlify-color-accent.svg" alt="Deploys by Netlify" /></a>',
+            },
+          ],
+        },
       ],
       copyright: `
         <br />
