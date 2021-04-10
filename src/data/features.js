958984dd04686cb62536a90b51118461d2df44a0
Lei Zhang <resouer@gmail.com>
2021-04-09 23:43:10 -0700

    Align with readme

--- src/data/features.js
+++ src/data/features.js
@@ -25,11 +25,11 @@ const features = [
       <>
         <p>
           <Translate>
-           In KubeVela, all its application deployment features such as workloads, operational behaviors, and cloud services
-            are defined via reusable </Translate><a href="https://github.com/cuelang/cue">
-              CUE</a> <Translate>and/or </Translate> <a href="https://helm.sh/">Helm</a><Translate> components. 
-              This creates a application deployment system whose capabilities are maintained as programmable packages, 
-              when your needs grow, your platform capabilities expand. Zero-restriction and fully flexible.</Translate>
+           KubeVela enables you to define platform features (such as workloads, operational behaviors, and cloud services)
+            as reusable </Translate><a href="https://github.com/cuelang/cue">
+              CUE</a> <Translate>and/or </Translate> <a href="https://helm.sh/">Helm</a><Translate> components, 
+              per needs of your application deployment. 
+              And when your needs grow, your platform capabilities expand naturally in a programmable approach.</Translate>
         </p>
       </>
     ),
@@ -42,10 +42,12 @@ const features = [
       <>
         <p>
           <Translate>
-            Though perfect in flexibility and best in developer experience, X-as-Code may leads to </Translate> <i>configuration drift</i> <Translate>. 
-            That's why KubeVela is built with Kubernetes, i.e. all its abstractions and capabilities are code but executed via battle tested 
+            Perfect in flexibility, X-as-Code tends to lead to configuration drift (i.e. the running instances are not in line with 
+              the expected configuration). That's why KubeVela is fully built with Kubernetes (instead of a client-side tool), 
+            i.e. all its abstractions and capabilities are modeled as code but enforced via battle tested 
             </Translate><a href="https://kubernetes.io/docs/concepts/architecture/controller/"> Kubernetes 
-            Control Loop</a><Translate>, convergence and determinism are guaranteed. Also, as a set of </Translate><a href="https://kubernetes.io/docs/concepts/extend-kubernetes/api-extension/custom-resources/">
+            Control Loop</a><Translate> which will never leave configuration drift in your clusters. Also, as a set of 
+            </Translate><a href="https://kubernetes.io/docs/concepts/extend-kubernetes/api-extension/custom-resources/">
              Custom Resources</a><Translate>, KubeVela seamlessly work with any CI/CD or GitOps tools, no integration burden.</Translate>
         </p>
       </>
