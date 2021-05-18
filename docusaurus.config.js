f39cdf4361497362feaab88280680103bb0c331e
guoxudong <guoxudong.dev@gmail.com>
2021-05-18 18:56:41 +0800

    fix edit page 404 (#109)
    * fix edit page 404
    
    * fix
    
    * fix
    
    * fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -151,17 +151,11 @@ module.exports = {
           showLastUpdateTime: true,
           includeCurrentVersion: true,
           lastVersion: 'v1.0',
-          // versions: {
-          //   current: {
-          //     label: 'master',
-          //     path: '/',
-          //   },
-          // },
         },
         blog: {
           showReadingTime: true,
           editUrl:
-            'https://github.com/oam-dev/kubevela.io/tree/main/blog',
+            'https://github.com/oam-dev/kubevela.io/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
