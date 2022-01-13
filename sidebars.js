8b100aaa0495c0cf1b40284cffd70b6e80743293
barnettZQG <barnett.zqg@gmail.com>
2022-01-13 20:32:54 +0800

    Docs: add ui schema doc (#434)
    Signed-off-by: barnettZQG <barnett.zqg@gmail.com>

--- sidebars.js
+++ sidebars.js
@@ -175,10 +175,7 @@ module.exports = {
         {
           type: "category",
           label: "Built-in Addons",
-          items: [
-            "reference/addons/overview",
-            "reference/addons/velaux",
-          ],
+          items: ["reference/addons/overview", "reference/addons/velaux"],
         },
         {
           type: "category",
@@ -223,11 +220,10 @@ module.exports = {
         {
           type: "category",
           label: "Built-in Policy Type",
-          items: [
-            "end-user/policies/envbinding",
-          ],
+          items: ["end-user/policies/envbinding"],
         },
         "end-user/workflow/built-in-workflow-defs",
+        "reference/ui-schema",
       ],
     },
     {
