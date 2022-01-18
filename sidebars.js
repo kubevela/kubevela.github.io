436a82f6efaf18988c543ed81edd2e6d99ecb8fb
barnettZQG <barnett.zqg@gmail.com>
2022-01-18 12:39:10 +0800

    Feat: change deploy docker image doc (#454)
    * Feat: change deploy docker image doc
    
    Signed-off-by: barnettZQG <barnett.zqg@gmail.com>
    
    * Docs: change the cluster version requirement
    
    Signed-off-by: barnettZQG <barnett.zqg@gmail.com>

--- sidebars.js
+++ sidebars.js
@@ -39,7 +39,7 @@ module.exports = {
         // TODO:
         {
           type: "category",
-          label: "Dashboard",
+          label: "VelaUX",
           collapsed: false,
           items: [
             {
