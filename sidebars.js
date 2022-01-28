30859c7591234bc5477b192a668f0ad44e909cd0
Zheng Xi Zhou <zzxwill@gmail.com>
2022-01-28 10:28:01 +0800

    Add reference doc for Tencent Cloud Subnet (#485)
    * Add reference doc for Tencent Cloud Subnet
    
    Added the reference doc for Tecent Cloud subnet
    
    Signed-off-by: Zheng Xi Zhou <zzxwill@gmail.com>
    
    * fix link issue
    
    Signed-off-by: Zheng Xi Zhou <zzxwill@gmail.com>

--- sidebars.js
+++ sidebars.js
@@ -191,6 +191,7 @@ module.exports = {
             "end-user/components/cloud-services/terraform/azure-database-mariadb",
             "end-user/components/cloud-services/terraform/azure-storage-account",
             "end-user/components/cloud-services/terraform/tencent-vpc",
+            "end-user/components/cloud-services/terraform/tencent-subnet",
           ],
         },
         {
