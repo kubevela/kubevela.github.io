5b1bf4428eb5685163d6777da3ae449191084286
Zheng Xi Zhou <zzxwill@gmail.com>
2022-06-17 15:59:58 +0800

    Merge four docs on database provisioning and usage (#722)
    * Merge four docs on database provisioning and usage
    
    Merged four docs into a landing page to show how to
    use databases in four ways
    
    Signed-off-by: Zheng Xi Zhou <zhengxi.zzx@alibaba-inc.com>
    
    * update docs in Chinese
    
    Signed-off-by: Zheng Xi Zhou <zhengxi.zzx@alibaba-inc.com>
    
    * fix CI
    
    Signed-off-by: Zheng Xi Zhou <zhengxi.zzx@alibaba-inc.com>
    
    * Fix CI issue
    
    Signed-off-by: Zheng Xi Zhou <zhengxi.zzx@alibaba-inc.com>
    
    * Update sidebars.js
    
    Co-authored-by: Jianbo Sun <wonderflow@icloud.com>
    
    * address comments
    
    Signed-off-by: Zheng Xi Zhou <zhengxi.zzx@alibaba-inc.com>
    
    * locallize `Manage Database`
    
    Signed-off-by: Zheng Xi Zhou <zhengxi.zzx@alibaba-inc.com>
    
    Co-authored-by: Zheng Xi Zhou <zhengxi.zzx@alibaba-inc.com>
    Co-authored-by: Jianbo Sun <wonderflow@icloud.com>

--- sidebars.js
+++ sidebars.js
@@ -41,12 +41,19 @@ module.exports = {
           items: ['tutorials/helm', 'tutorials/helm-multi-cluster'],
         },
         {
-          'Cloud Resources CD': [
+          type: 'category',
+          label: 'Cloud Resources CD',
+          collapsed: true,
+          items: [
             'tutorials/consume-cloud-services',
-            'end-user/components/cloud-services/provision-and-consume-cloud-services',
-            'end-user/components/cloud-services/provision-and-initiate-database',
-            'end-user/components/cloud-services/secure-your-database-connection',
-            'end-user/components/cloud-services/provision-an-RDS-instance-with-more-than-one-database',
+            {
+            'Manage Database': [
+              'end-user/components/cloud-services/provision-and-consume-cloud-services',
+              'end-user/components/cloud-services/provision-and-initiate-database',
+              'end-user/components/cloud-services/secure-your-database-connection',
+              'end-user/components/cloud-services/provision-an-RDS-instance-with-more-than-one-database'
+            ],
+          }
           ],
         },
         {
