2d6150530b9935fae6b1036d1f25588e7345c1ee
Ihor Sychevskyi <arhell333@gmail.com>
2022-11-07 14:35:22 +0200

    remove plugin (#1058)
    Signed-off-by: Arhell <arhell333@gmail.com>
    
    Signed-off-by: Arhell <arhell333@gmail.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -166,6 +166,79 @@ module.exports = {
       },
     },
   },
+  headTags: [
+    {
+      tagName: "link",
+      attributes: {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/img/favicons/apple-touch-icon.png",
+      },
+    },
+    {
+      tagName: "link",
+      attributes: {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/img/favicons/favicon-32x32.png",
+      },
+    },
+    {
+      tagName: "link",
+      attributes: {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/img/favicons/favicon-16x16.png",
+      },
+    },
+    {
+      tagName: "link",
+      attributes: {
+        rel: "icon",
+        type: "image/svg+xml",
+        href: "/img/favicons/favicon.svg",
+      },
+    },
+    {
+      tagName: "link",
+      attributes: {
+        rel: "icon",
+        type: "image/png",
+        href: "/img/favicons/favicon.png",
+      },
+    },
+    {
+      tagName: "link",
+      attributes: {
+        rel: "manifest",
+        href: "/img/favicons/site.webmanifest",
+      },
+    },
+    {
+      tagName: "link",
+      attributes: {
+        rel: "mask-icon",
+        color: "#ffffff",
+        href: "/img/favicons/safari-pinned-tab.svg",
+      },
+    },
+    {
+      tagName: "meta",
+      attributes: {
+        name: "theme-color",
+        content: "#ffffff",
+      },
+    },
+    {
+      tagName: "meta",
+      attributes: {
+        name: "msapplication-config",
+        content: "/img/favicons/browserconfig.xml",
+      },
+    },
+  ],
   presets: [
     [
       '@docusaurus/preset-classic',
@@ -216,7 +289,6 @@ module.exports = {
       },
     ],
     'docusaurus-plugin-sass',
-    './src/plugins/faviconCustomPlugin',
     require.resolve('docusaurus-plugin-image-zoom'),
   ],
   scripts: [
